dd6f4473d85564ea676b723198b9fad418efd483
Rakibul Hasan <rifat20089@hotmail.com>
2021-04-16 17:53:27 +0600

    slider done

---
+++ js/slider.js
@@ -0,0 +1,15 @@
+// Glider js carousel
+const slider1 = document.getElementById('glide_1');
+
+// main 
+if(slider1){
+    new Glide(slider1,{
+        type: 'carousel',
+        startAt: 0,
+        // autoplay: 3000,
+        hoverpause: true,
+        perView: 1,
+        animationDuration: 800,
+        animationTimingFunc: 'liner'
+    }).mount();
+}
